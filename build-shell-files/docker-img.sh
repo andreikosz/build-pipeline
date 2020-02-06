756e83486c2d1bd0d8e9a18df9518b657e551c1b
Andrei Koszorus <andrei.koszorus@accesa.eu>
2020-02-06 13:21:06 +0200

    new

--- build-shell-files/docker-img.sh
+++ build-shell-files/docker-img.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 if cd java-vm-deploy && test -f Dockerfile; then
     echo "Using custom Dockerfile"
-    if  docker build -t gcr.io/terraform-265913/java-app . && docker push gcr.io/terraform-265913/java-app && docker run gcr.io/terraform-265913/java-app;then
+    if  docker build -t gcr.io/terraform-265913/java-app:latest . && docker push gcr.io/terraform-265913/java-app:test;then
         echo "Docker image pushed"
     else
         echo "Error at building docker image" && exit 1
     fi
 else
     echo "Using default Dockerfile"
-    if  cd .. && docker build -f build-pipeline/default-build-files/Dockerfile -t gcr.io/terraform-265913/java-app:latest . && docker push gcr.io/terraform-265913/java-app:latest ;then
+    if  cd .. && docker build -f build-pipeline/default-build-files/Dockerfile -t gcr.io/terraform-265913/java-app:latest . && docker push gcr.io/terraform-265913/java-app:latest && docker run gcr.io/terraform-265913/java-app:latest;then
         echo "Docker image pushed"
     else
         echo "Error at building docker image" && exit 1
