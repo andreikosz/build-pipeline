e9f5f046826d58b22689197903cd8c7ffddd57cc
Andrei Koszorus <andrei.koszorus@accesa.eu>
2020-02-18 14:19:47 +0200

    new

--- build-shell-files/run.sh
+++ build-shell-files/run.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
-if !(fuser -k 8080/tcp);then
+if ! (fuser -k 8080/tcp);then
     echo "Could not kill app"
 else
     echo "App succesfully killed"
 fi
 
-if [[ $1="java" ]];then
+if [[ $1 = "java" ]];then
     if ! ( gcloud compute ssh andreikosz_96@my-instance-2 --zone=us-central1-a --command="java -jar spring-boot-app.jar 2 > output.txt &");then
         echo "Could not run java app" && exit 1
     else
         echo "App is running on VM external IP port 8080"
     fi
-elif [[ $1="python" ]]
+elif [[ $1 = "python" ]];then
     if ! (gcloud compute ssh andreikosz_96@my-instance-2 --zone=us-central1-a --command="python backend.py 2 > output.txt &");then
          echo "Could not run pytho app" && exit 1
     else
