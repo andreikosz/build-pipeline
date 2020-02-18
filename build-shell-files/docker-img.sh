caef318e4f5afbb92dcd6f7b30eab80f912864a0
Andrei Koszorus <andrei.koszorus@accesa.eu>
2020-02-18 16:51:07 +0200

    new

--- build-shell-files/docker-img.sh
+++ build-shell-files/docker-img.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-if cd java-vm-deploy && test -f Dockerfile; then
+if cd app-folder && test -f Dockerfile; then
     echo "Using custom Dockerfile"
     if  docker build -t gcr.io/terraform-265913/app:latest . && docker push gcr.io/terraform-265913/app:latest;then
         echo "Docker image pushed"
@@ -14,7 +14,7 @@ else
         else
          echo "Error at building docker image" && exit 1
         fi
-    elif [[ $1 = "python" ]]
+    elif [[ $1 = "python" ]];then
         if  cd .. && docker build -f build-pipeline/default-build-files/python/Dockerfile -t gcr.io/terraform-265913/app:latest . && docker push gcr.io/terraform-265913/app:latest;then
         echo "Docker image pushed"
         else
