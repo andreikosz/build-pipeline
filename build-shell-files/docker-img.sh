f1fa13b838af14660401d3e76ebc53c5403ec184
Andrei Koszorus <andrei.koszorus@accesa.eu>
2020-02-19 15:16:24 +0200

    new

--- build-shell-files/docker-img.sh
+++ build-shell-files/docker-img.sh
@@ -4,10 +4,10 @@ if cd app-folder && test -f Dockerfile && test -f kubernetes.yaml.tpl; then
     DOCKER_IMG_BASE="gcr.io/terraform-265913/app:"
     NEW_UUID=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 5)
     DOCKER_IMG_FULL_NAME="$DOCKER_IMG_BASE$NEW_UUID"
-    SED_REPLACEMENT = """s/DOCKER_IMG/"$
+    SED_REPLACEMENT = ""
     if  docker build -t $DOCKER_IMG_FULL_NAME . && docker push $DOCKER_IMG_FULL_NAME;then
         echo "Docker image pushed"
-        if sed "s/DOCKER_IMG/${DOCKER_IMG_FULL_NAME}/g" kubernetes.yaml.tpl > kubernetes.yaml;then
+        if sed "s/DOCKER_IMG/${DOCKER_IMG_FULL_NAME}" kubernetes.yaml.tpl > kubernetes.yaml;then
             echo "Generate kubernetes.yaml file"
         else
             echo "Failed to generate yaml file" && exit 1
