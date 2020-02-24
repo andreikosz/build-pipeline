60211a32ad51097abf67a64b94ca722ef52f49b5
Andrei Koszorus <andrei.koszorus@accesa.eu>
2020-02-24 16:44:33 +0200

    new

--- build-shell-files/checkout-code.sh
+++ build-shell-files/checkout-code.sh
@@ -2,5 +2,5 @@
 if [[ $1 = "java8" ]];then
    source build-pipeline/build-shell-files/checkout-code-shells/checkout-java8-app.sh
 elif [[ $1 = "python3" ]];then
-    source build-pipeline/build-shell-files/docker-shells/checkout-python3-app.sh
+    source build-pipeline/build-shell-files/checkout-code-shells/checkout-python3-app.sh
 fi
