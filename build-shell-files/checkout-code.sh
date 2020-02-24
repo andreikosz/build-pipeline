7a8276e493b3b4880a04647a375ff394fd9e2ce7
Andrei Koszorus <andrei.koszorus@accesa.eu>
2020-02-24 13:02:41 +0200

    new

--- build-shell-files/checkout-code.sh
+++ build-shell-files/checkout-code.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
-if ! git clone https://github.com/andreikosz/java-vm-deploy.git app-folder;then
-    echo "Could not clone code repo" && exit 1
-else
-    echo "Code repo cloned"
+if [[ $1 = "java" ]];then
+    if ! git clone https://github.com/andreikosz/java-vm-deploy.git app-folder;then
+         echo "Could not clone code repo" && exit 1
+    else
+        echo "Code repo cloned"
+    fi
+elif [[ $1 = "python" ]];then
+    if ! git clone https://github.com/andreikosz/python-web-app.git app-folder;then
+         echo "Could not clone code repo" && exit 1
+    else
+        echo "Code repo cloned"
+    fi
 fi
