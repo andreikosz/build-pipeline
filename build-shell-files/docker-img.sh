1858f52b4210ab692bdee94239a2e74b6c421130
Andrei Koszorus <andrei.koszorus@accesa.eu>
2020-02-25 09:51:12 +0200

    new

--- build-shell-files/docker-img.sh
+++ build-shell-files/docker-img.sh
@@ -18,8 +18,8 @@ if cd app-folder && test -f Dockerfile && test -f kubernetes.yaml.tpl; then
 else
     echo "Using default Dockerfile"
     if [[ $1 = "java8" ]]; then
-        cd .. && ls . && source build-pipeline/build-shell-files/docker-shells/java8-docker.sh
+        cd .. && ls . && source ./build-pipeline/build-shell-files/docker-shells/java8-docker.sh
     elif [[ $1 = "python3" ]];then
-        cd .. && ls . && source build-pipeline/build-shell-files/docker-shells/python3-docker.sh
+        cd .. && ls . && source ./build-pipeline/build-shell-files/docker-shells/python3-docker.sh
     fi
 fi
