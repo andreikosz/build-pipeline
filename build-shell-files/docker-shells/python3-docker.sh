ba037c2e606a88da08e1c18566d7a2e17251e8b4
Andrei Koszorus <andrei.koszorus@accesa.eu>
2020-02-25 09:56:09 +0200

    new

--- build-shell-files/docker-shells/python3-docker.sh
+++ build-shell-files/docker-shells/python3-docker.sh
@@ -1,4 +1,4 @@
-if  cd .. && docker build -f build-pipeline/default-build-files/python/Dockerfile -t gcr.io/terraform-265913/app:latest . && docker push gcr.io/terraform-265913/app:latest;then
+if  docker build -f build-pipeline/default-build-files/python/Dockerfile -t gcr.io/terraform-265913/app:latest . && docker push gcr.io/terraform-265913/app:latest;then
     echo "Docker image pushed"
 else
     echo "Error at building docker image" && exit 1
