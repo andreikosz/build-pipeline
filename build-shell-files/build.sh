832804ffa3cdbba2eb7156091ebf49ba6aa61878
Andrei Koszorus <andrei.koszorus@accesa.eu>
2020-02-28 16:54:18 +0200

    new

--- build-shell-files/build.sh
+++ build-shell-files/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-if [[ $1 = "java" ]];then
+if [[ $1 = "java8" ]];then
     source build-pipeline/build-shell-files/java-shells/build-java8.sh
-elif [[ $1 = "python" ]];then
+elif [[ $1 = "python3" ]];then
     source build-pipeline/build-shell-files/python-shells/build-python3.sh
 fi
