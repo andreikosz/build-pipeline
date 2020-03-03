c319bf1f89a017a3111c94f670135cecbf6c58de
Andrei Koszorus <andrei.koszorus@accesa.eu>
2020-03-03 11:47:31 +0200

    new

--- build-shell-files/infra.sh
+++ build-shell-files/infra.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-if ! docker run gcr.io/terraform-265913/terraform-vm ${1} ${2}; then
+if ! docker run gcr.io/terraform-265913/terraform-vm ${1} ${2} ${3}; then
     echo "Could not create infrastructure object" && exit 1
 else
     echo "Vm instance created" 
