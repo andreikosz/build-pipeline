1b3884151106b40505873be074e5cb98860a8d92
Andrei Koszorus <andrei.koszorus@accesa.eu>
2020-03-05 14:36:48 +0200

    new

--- build-shell-files/infra-dns.sh
+++ build-shell-files/infra-dns.sh
@@ -1,4 +1,4 @@
-if ! docker run gcr.io/terraform-265913/terraform-dns ${1}; then
+if ! ls . && docker run gcr.io/terraform-265913/terraform-dns ${1}; then
     echo "Could not create DNS entry" && exit 1
 else
     echo "DNS Entry created" 
