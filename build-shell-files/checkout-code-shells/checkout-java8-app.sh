17f3825c6a9e96d9499c4b2d02f04c2b64a96610
andrei.koszorus <andreikosz>
2020-03-19 18:01:34 +0200

    dynamic checkout-code

--- build-shell-files/checkout-code-shells/checkout-java8-app.sh
+++
@@ -1,5 +0,0 @@
- if ! git clone https://github.com/andreikosz/java-vm-deploy.git app-folder;then
-    echo "Could not clone code repo" && exit 1
-else
-    echo "Code repo cloned"
-fi
--- build-shell-files/checkout-code-shells/checkout-python3-app.sh
+++
@@ -1,5 +0,0 @@
-if ! git clone https://github.com/andreikosz/python-web-app.git app-folder;then
-    echo "Could not clone code repo" && exit 1
-else
-    echo "Code repo cloned"
-fi
